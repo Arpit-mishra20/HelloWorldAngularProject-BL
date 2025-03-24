8095b97398330270a2aa45a3423340a0b49309dd
Arpit-mishra20 <arpit.202033@gmail.com>
2025-03-24 11:51:22 +0530

    Added UC4 code

--- Helloworld/src/app/app-routing.module.ts
+++ Helloworld/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations:[
+    AppComponent
+  ],
+
+  imports: [AppRoutingModule,
+    BrowserModule,
+    FormsModule
+  ],
+  providers:[],
+  bootstrap:[AppComponent]
+
 })
 export class AppRoutingModule { }
